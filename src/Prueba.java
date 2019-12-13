d2a6f5bca5eaa6eea98e5b01fbf300223de9c516
Carlos <Carlos@CARLOS>
2019-12-13 19:44:03 +0100

    Suma

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,8 +1,14 @@
 
 public class Prueba {
 
+	public static void sumar (int num, int numero) {
+		int resultado= num+numero;
+		System.out.println("El resultado de la suma es "+resultado);
+	}
 	public static void main(String[] args) {
-		System.out.println("Holiiii");
+		int num=2;
+		int numero=4;
+		sumar(num,numero);
 
 	}
 
