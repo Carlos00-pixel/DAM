b4ecb4a44040423ff9b3d6db0757409a78d76b14
Carlos <Carlos@CARLOS>
2019-12-13 19:51:33 +0100

    Multiplicacion

--- src/Prueba.java
+++ src/Prueba.java
@@ -9,11 +9,16 @@ public class Prueba {
 		int resultado= num-numero;
 		System.out.println("El resultado de la resta es "+resultado);
 	}
+	public static void multiplicar (int num, int numero) {
+		int resultado= num*numero;
+		System.out.println("El resultado de la multiplicacion es "+resultado);
+	}
 	public static void main(String[] args) {
 		int num=2;
 		int numero=4;
 		sumar(num,numero);
 		restar(num, numero);
+		multiplicar(num, numero);
 	}
 
 }
