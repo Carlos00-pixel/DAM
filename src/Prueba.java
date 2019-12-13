21024757350b37492a0c868d99d57585d250236c
Carlos <Carlos@CARLOS>
2019-12-13 19:47:29 +0100

    Resta

--- src/Prueba.java
+++ src/Prueba.java
@@ -5,11 +5,15 @@ public class Prueba {
 		int resultado= num+numero;
 		System.out.println("El resultado de la suma es "+resultado);
 	}
+	public static void restar (int num, int numero) {
+		int resultado= num-numero;
+		System.out.println("El resultado de la resta es "+resultado);
+	}
 	public static void main(String[] args) {
 		int num=2;
 		int numero=4;
 		sumar(num,numero);
-
+		restar(num, numero);
 	}
 
 }
