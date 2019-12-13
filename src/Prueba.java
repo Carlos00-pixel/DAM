43560fb9cf29d363e26d16fa99933c1e93992c90
Carlos <Carlos@CARLOS>
2019-12-13 19:52:35 +0100

    Division

--- src/Prueba.java
+++ src/Prueba.java
@@ -13,12 +13,17 @@ public class Prueba {
 		int resultado= num*numero;
 		System.out.println("El resultado de la multiplicacion es "+resultado);
 	}
+	public static void division (int num, int numero) {
+		int resultado= num/numero;
+		System.out.println("El resultado de la multiplicacion es "+resultado);
+	}
 	public static void main(String[] args) {
 		int num=2;
 		int numero=4;
 		sumar(num,numero);
 		restar(num, numero);
 		multiplicar(num, numero);
+		division(num, numero);
 	}
 
 }
